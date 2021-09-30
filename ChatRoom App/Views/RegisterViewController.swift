78297ba2a583e1e489aa594220545e1ff774cb9d
fatemeh <fatemeh.ka84@gmail.com>
2021-09-30 15:35:28 -0400

    pod changes

---
+++ ChatRoom App/Views/RegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterViewController.swift
+//  ChatRoom App
+//
+//  Created by Fateme Karimi on 2021-09-15.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+
+platform :ios, '13.0'
+
+target 'ChatRoom App' do
+ 
+  use_frameworks!
+
+  # Pods for ChatRoom App
+ pod 'CLTypingLabel', '~> 0.4.0'
+
+end
